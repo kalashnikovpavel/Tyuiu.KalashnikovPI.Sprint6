7245fbaac42fd0ffdc83d6510a2b30864523619d
kalashnikovpi <kalashnicovp@yandex.ru>
2024-12-10 15:46:31 +0500

    Task4

--- Tyuiu.KalashnikovPI.Sprint6.Task4.V8.Lib/DataService.cs
+++ Tyuiu.KalashnikovPI.Sprint6.Task4.V8.Lib/DataService.cs
@@ -12,7 +12,15 @@ namespace Tyuiu.KalashnikovPI.Sprint6.Task4.V8.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round((Math.Sin(x) + ((Math.Cos(x)+1)/(2-x))+2*x), 2);
+                if (x == 2)
+                {
+                    // Обработка случая x = 2. Можно, например, присвоить y какое-то значение по умолчанию или бросить исключение.
+                    y = 0.0; // или другое подходящее значение
+                }
+                else
+                {
+                    y = Math.Round((Math.Sin(x) + ((Math.Cos(x) + 1) / (2 - x)) + 2 * x), 2);
+                }
                 valueArray[count] = y;
                 count++;
             }
